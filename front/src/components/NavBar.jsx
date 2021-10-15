40d1fb45f0bd862de5926c8f0e735429f8f18338
Francois Janson <jansonfrancois@gmail.com>
2021-10-15 14:56:47 +0200

    react navbar

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <Link className="navbar-brand" href="#">
         Express_Login
-      </a>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
